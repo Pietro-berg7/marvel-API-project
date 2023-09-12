ba98a5e9cc106d49843a6e1cc284ccaa3eef2d46
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-09-11 22:05:25 -0300

    add database context

---
+++ marvel-API-project/src/Database/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using marvel_API_project.src.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace marvel_API_project.src.Database
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Hero> Heroes { get; set; }
+        public DbSet<Group> Groups { get; set; }
+    }
+}
