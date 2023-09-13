d7d37fc550a0942a1b3540fd2e7fd1af7003d8c9
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-09-13 13:36:54 -0300

    Add database connection

--- marvel-API-project/Program.cs
+++ marvel-API-project/Program.cs
@@ -1,4 +1,12 @@
+using marvel_API_project.src.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
 
 // Add services to the container.
 
@@ -6,6 +14,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = configuration.GetSection("ConnectionStrings:DefaultConnection");
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString.Value));
+builder.Services.AddScoped<DataContext, DataContext>();
 
 var app = builder.Build();
 
